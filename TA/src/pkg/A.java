f3b5d0dfc10e57c39acfa895f2d05b2346b4d159
MinakshiChaudhary1 <minakshichaudhary26@gmail.com>
2019-06-02 22:31:47 +0530

    Added New Test Case

--- TA/src/pkg/A.java
+++ TA/src/pkg/A.java
@@ -8,6 +8,10 @@ public int add(int a, int b) {
 	int c=a+b;
 	return c;
 }
+public int sub(int a, int b) {
+	int c=a-b;
+	return c;
+}
 public void mul(int a, int b) {
 	int c=a*b;
 	System.out.println(c);
