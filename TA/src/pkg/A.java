3bdace25a6a5cc07776f7e830eddbc0b77b229bd
MinakshiChaudhary1 <minakshichaudhary26@gmail.com>
2019-06-02 19:32:06 +0530

    Auto Project started

---
+++ TA/src/pkg/A.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class A {
+	public A(int s){
+		System.out.println("Welcome");
+	}
+public int add(int a, int b) {
+	int c=a+b;
+	return c;
+}
+public void mul(int a, int b) {
+	int c=a*b;
+	System.out.println(c);
+}
+}
